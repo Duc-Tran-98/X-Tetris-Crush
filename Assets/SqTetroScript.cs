44baf103f40355da71c5588d490d9fca866aab4a
Duc Tran <dt135@duke.edu>
2021-04-06 19:56:57 -0400

    First Commit

---
+++ Assets/SqTetroScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SqTetroScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color tetroColor = new Color(
+            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f)
+        );
+        changeTetroColor(tetroColor);
+    }
+
+    void changeTetroColor(Color newColor)
+    {
+        Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in childRenderers){
+            rend.material.color = newColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
